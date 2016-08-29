55fc95662b2b65d803cc5fed95a799d1d95f326b
brnkhy <brnkahyaoglu@gmail.com>
2016-08-29 23:28:53 +0300

    Add CachedDynamicTileManager for offline usage
    Every json pulled from Mapzen api saved into "/MapzenGo/CachedTileData/" folder for later usage. Then it checks that folder for json before requesting from mapzen api.
    Fixes #20

---
+++ Assets/MapzenGo/Models/CachedDynamicTileManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Assets.Helpers;
+using Assets.Models.Factories;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+namespace Assets.Models
+{
+    public class CachedDynamicTileManager : DynamicTileManager
+    {
+        protected string CacheFolderPath;
+
+        public override void Init(List<Factory> factories, World.Settings settings)
+        {
+            CacheFolderPath = Path.Combine(Application.dataPath, "MapzenGo/CachedTileData/");
+            if (!Directory.Exists(CacheFolderPath))
+                Directory.CreateDirectory(CacheFolderPath);
+            base.Init(factories, settings);
+        }
+
+        protected override void LoadTile(Vector2 tileTms, Tile tile)
+        {
+            var url = string.Format(_mapzenUrl, _mapzenLayers, Zoom, tileTms.x, tileTms.y, _mapzenFormat, _key);
+
+            var tilePath = Path.Combine(CacheFolderPath, Zoom + "_" + tileTms.x + "_" + tileTms.y);
+            string mapData;
+            if (File.Exists(tilePath))
+            {
+                var r = new StreamReader(tilePath, Encoding.Default);
+                mapData = r.ReadToEnd();
+                tile.ConstructTile(mapData);
+            }
+            else
+            {
+                ObservableWWW.Get(url).Subscribe(
+                    success =>
+                    {
+                        var sr = File.CreateText(tilePath);
+                        sr.Write(success);
+                        sr.Close();
+                        tile.ConstructTile(success);
+                    },
+                    error =>
+                    {
+                        Debug.Log(error);
+                    });
+            }
+        }
+    }
+}
