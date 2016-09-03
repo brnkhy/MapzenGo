2ffa34485683b4b119df7ba305cae6bc7328caa4
brnkhy <brnkahyaoglu@gmail.com>
2016-09-03 03:25:26 +0300

    Extend landuse factory to use a much more detailed settings system
    Now it'll draw the landuse types which has a "setting" defined under landuse factory. IF you have just parks there, it'll only draw parks. Parks and residental? Then parks and residential areas.

---
+++ Assets/MapzenGo/Models/Landuse.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Assets.Helpers;
+using Assets.MapzenGo.Models.Enums;
+using UnityEngine;
+
+namespace Assets.Models
+{
+    [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+    public class Landuse : MonoBehaviour
+    {
+        public string Id;
+        public string Type;
+        public LanduseKind Kind;
+        public string Name;
+        public int SortKey;
+        
+        [Serializable]
+        public class Settings
+        {
+            public List<LanduseSettings> AllSettings;
+
+            public LanduseSettings GetSettingsFor(LanduseKind type)
+            {
+                return AllSettings.FirstOrDefault(x => x.Type == type);
+            }
+        }
+
+        [Serializable]
+        public class LanduseSettings
+        {
+            public LanduseKind Type;
+            public Material Material;
+        }
+
+    }
+
+}
