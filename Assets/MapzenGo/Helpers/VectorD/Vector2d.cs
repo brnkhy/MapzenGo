decd22f244fdd350688479432a63de3871b32a87
brnkhy <brnkahyaoglu@gmail.com>
2016-09-11 02:01:21 +0300

    Changed code style and tostring function of Vector2d

--- Assets/MapzenGo/Helpers/VectorD/Vector2d.cs
+++ Assets/MapzenGo/Helpers/VectorD/Vector2d.cs
@@ -4,15 +4,20 @@
 using System;
 using System.Runtime.CompilerServices;
 
-namespace UnityEngine {
-    public struct Vector2d {
+namespace UnityEngine
+{
+    public struct Vector2d
+    {
         public const double kEpsilon = 1E-05d;
         public double x;
         public double y;
 
-        public double this[int index] {
-            get {
-                switch (index) {
+        public double this[int index]
+        {
+            get
+            {
+                switch (index)
+                {
                     case 0:
                         return this.x;
                     case 1:
@@ -21,8 +26,10 @@ namespace UnityEngine {
                         throw new IndexOutOfRangeException("Invalid Vector2d index!");
                 }
             }
-            set {
-                switch (index) {
+            set
+            {
+                switch (index)
+                {
                     case 0:
                         this.x = value;
                         break;
@@ -35,106 +42,134 @@ namespace UnityEngine {
             }
         }
 
-        public Vector2d normalized {
-            get {
+        public Vector2d normalized
+        {
+            get
+            {
                 Vector2d vector2d = new Vector2d(this.x, this.y);
                 vector2d.Normalize();
                 return vector2d;
             }
         }
 
-        public double magnitude {
-            get {
+        public double magnitude
+        {
+            get
+            {
                 return Mathd.Sqrt(this.x * this.x + this.y * this.y);
             }
         }
 
-        public double sqrMagnitude {
-            get {
+        public double sqrMagnitude
+        {
+            get
+            {
                 return this.x * this.x + this.y * this.y;
             }
         }
 
-        public static Vector2d zero {
-            get {
+        public static Vector2d zero
+        {
+            get
+            {
                 return new Vector2d(0.0d, 0.0d);
             }
         }
 
-        public static Vector2d one {
-            get {
+        public static Vector2d one
+        {
+            get
+            {
                 return new Vector2d(1d, 1d);
             }
         }
 
-        public static Vector2d up {
-            get {
+        public static Vector2d up
+        {
+            get
+            {
                 return new Vector2d(0.0d, 1d);
             }
         }
 
-        public static Vector2d right {
-            get {
+        public static Vector2d right
+        {
+            get
+            {
                 return new Vector2d(1d, 0.0d);
             }
         }
 
-        public Vector2d(double x, double y) {
+        public Vector2d(double x, double y)
+        {
             this.x = x;
             this.y = y;
         }
-        
-        public static implicit operator Vector2d(Vector3d v) {
+
+        public static implicit operator Vector2d(Vector3d v)
+        {
             return new Vector2d(v.x, v.y);
         }
 
-        public static implicit operator Vector3d(Vector2d v) {
+        public static implicit operator Vector3d(Vector2d v)
+        {
             return new Vector3d(v.x, v.y, 0.0d);
         }
 
-        public static Vector2d operator +(Vector2d a, Vector2d b) {
+        public static Vector2d operator +(Vector2d a, Vector2d b)
+        {
             return new Vector2d(a.x + b.x, a.y + b.y);
         }
 
-        public static Vector2d operator -(Vector2d a, Vector2d b) {
+        public static Vector2d operator -(Vector2d a, Vector2d b)
+        {
             return new Vector2d(a.x - b.x, a.y - b.y);
         }
 
-        public static Vector2d operator -(Vector2d a) {
+        public static Vector2d operator -(Vector2d a)
+        {
             return new Vector2d(-a.x, -a.y);
         }
 
-        public static Vector2d operator *(Vector2d a, double d) {
+        public static Vector2d operator *(Vector2d a, double d)
+        {
             return new Vector2d(a.x * d, a.y * d);
         }
 
-        public static Vector2d operator *(float d, Vector2d a) {
+        public static Vector2d operator *(float d, Vector2d a)
+        {
             return new Vector2d(a.x * d, a.y * d);
         }
 
-        public static Vector2d operator /(Vector2d a, double d) {
+        public static Vector2d operator /(Vector2d a, double d)
+        {
             return new Vector2d(a.x / d, a.y / d);
         }
 
-        public static bool operator ==(Vector2d lhs, Vector2d rhs) {
+        public static bool operator ==(Vector2d lhs, Vector2d rhs)
+        {
             return Vector2d.SqrMagnitude(lhs - rhs) < 0.0 / 1.0;
         }
 
-        public static bool operator !=(Vector2d lhs, Vector2d rhs) {
+        public static bool operator !=(Vector2d lhs, Vector2d rhs)
+        {
             return (double)Vector2d.SqrMagnitude(lhs - rhs) >= 0.0 / 1.0;
         }
 
-        public void Set(double new_x, double new_y) {
+        public void Set(double new_x, double new_y)
+        {
             this.x = new_x;
             this.y = new_y;
         }
 
-        public static Vector2d Lerp(Vector2d from, Vector2d to, double t) {
+        public static Vector2d Lerp(Vector2d from, Vector2d to, double t)
+        {
             t = Mathd.Clamp01(t);
             return new Vector2d(from.x + (to.x - from.x) * t, from.y + (to.y - from.y) * t);
         }
 
-        public static Vector2d MoveTowards(Vector2d current, Vector2d target, double maxDistanceDelta) {
+        public static Vector2d MoveTowards(Vector2d current, Vector2d target, double maxDistanceDelta)
+        {
             Vector2d vector2 = target - current;
             double magnitude = vector2.magnitude;
             if (magnitude <= maxDistanceDelta || magnitude == 0.0d)
@@ -143,16 +178,19 @@ namespace UnityEngine {
                 return current + vector2 / magnitude * maxDistanceDelta;
         }
 
-        public static Vector2d Scale(Vector2d a, Vector2d b) {
+        public static Vector2d Scale(Vector2d a, Vector2d b)
+        {
             return new Vector2d(a.x * b.x, a.y * b.y);
         }
 
-        public void Scale(Vector2d scale) {
+        public void Scale(Vector2d scale)
+        {
             this.x *= scale.x;
             this.y *= scale.y;
         }
 
-        public void Normalize() {
+        public void Normalize()
+        {
             double magnitude = this.magnitude;
             if (magnitude > 9.99999974737875E-06)
                 this = this / magnitude;
@@ -160,23 +198,25 @@ namespace UnityEngine {
                 this = Vector2d.zero;
         }
 
-        public override string ToString() {
-            /*
-      string fmt = "({0:D1}, {1:D1})";
-      object[] objArray = new object[2];
-      int index1 = 0;
-      // ISSUE: variable of a boxed type
-      __Boxed<double> local1 = (ValueType) this.x;
-      objArray[index1] = (object) local1;
-      int index2 = 1;
-      // ISSUE: variable of a boxed type
-      __Boxed<double> local2 = (ValueType) this.y;
-      objArray[index2] = (object) local2;
-      */
+        public override string ToString()
+        {
+            return this.x + " - " + this.y;
+            //string fmt = "({0:D1}, {1:D1})";
+            //object[] objArray = new object[2];
+            //int index1 = 0;
+            //// ISSUE: variable of a boxed type
+            //__Boxed<double> local1 = (ValueType)this.x;
+            //objArray[index1] = (object)local1;
+            //int index2 = 1;
+            //// ISSUE: variable of a boxed type
+            //__Boxed<double> local2 = (ValueType)this.y;
+            //objArray[index2] = (object)local2;
+
             return "not implemented";
         }
 
-        public string ToString(string format) {
+        public string ToString(string format)
+        {
             /* TODO:
       string fmt = "({0}, {1})";
       object[] objArray = new object[2];
@@ -190,11 +230,13 @@ namespace UnityEngine {
             return "not implemented";
         }
 
-        public override int GetHashCode() {
+        public override int GetHashCode()
+        {
             return this.x.GetHashCode() ^ this.y.GetHashCode() << 2;
         }
 
-        public override bool Equals(object other) {
+        public override bool Equals(object other)
+        {
             if (!(other is Vector2d))
                 return false;
             Vector2d vector2d = (Vector2d)other;
@@ -204,38 +246,46 @@ namespace UnityEngine {
                 return false;
         }
 
-        public static double Dot(Vector2d lhs, Vector2d rhs) {
+        public static double Dot(Vector2d lhs, Vector2d rhs)
+        {
             return lhs.x * rhs.x + lhs.y * rhs.y;
         }
 
-        public static double Angle(Vector2d from, Vector2d to) {
+        public static double Angle(Vector2d from, Vector2d to)
+        {
             return Mathd.Acos(Mathd.Clamp(Vector2d.Dot(from.normalized, to.normalized), -1d, 1d)) * 57.29578d;
         }
 
-        public static double Distance(Vector2d a, Vector2d b) {
+        public static double Distance(Vector2d a, Vector2d b)
+        {
             return (a - b).magnitude;
         }
 
-        public static Vector2d ClampMagnitude(Vector2d vector, double maxLength) {
+        public static Vector2d ClampMagnitude(Vector2d vector, double maxLength)
+        {
             if (vector.sqrMagnitude > maxLength * maxLength)
                 return vector.normalized * maxLength;
             else
                 return vector;
         }
 
-        public static double SqrMagnitude(Vector2d a) {
+        public static double SqrMagnitude(Vector2d a)
+        {
             return (a.x * a.x + a.y * a.y);
         }
 
-        public double SqrMagnitude() {
+        public double SqrMagnitude()
+        {
             return (this.x * this.x + this.y * this.y);
         }
 
-        public static Vector2d Min(Vector2d lhs, Vector2d rhs) {
+        public static Vector2d Min(Vector2d lhs, Vector2d rhs)
+        {
             return new Vector2d(Mathd.Min(lhs.x, rhs.x), Mathd.Min(lhs.y, rhs.y));
         }
 
-        public static Vector2d Max(Vector2d lhs, Vector2d rhs) {
+        public static Vector2d Max(Vector2d lhs, Vector2d rhs)
+        {
             return new Vector2d(Mathd.Max(lhs.x, rhs.x), Mathd.Max(lhs.y, rhs.y));
         }
     }
