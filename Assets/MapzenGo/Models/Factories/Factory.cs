174975667b7a310cfe6fa8d97902bb40032f1ef7
brnkhy <brnkahyaoglu@gmail.com>
2016-09-15 15:16:22 +0300

    add "just draw everything fam" option
    to draw everything regardless of settings/filtering. useful if you havent memorized all types & enums.

--- Assets/MapzenGo/Models/Factories/Factory.cs
+++ Assets/MapzenGo/Models/Factories/Factory.cs
@@ -9,6 +9,7 @@ namespace MapzenGo.Models.Factories
     public class Factory : Plugin
     {
         public bool MergeMeshes;
+        public bool JustDrawEverythingFam = false;
         public float Order = 1;
         public virtual string XmlTag {get { return ""; } }
         public virtual Func<JSONObject, bool> Query { get; set; }
