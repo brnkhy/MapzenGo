525c71ed43e92ad794fcb5f1c4f285355bf85ed6
brnkhy <brnkahyaoglu@gmail.com>
2016-09-27 19:57:58 +0300

    add new poi prefab and icon

---
+++ Assets/MapzenGo/Helpers/Billboard.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Billboard : MonoBehaviour
+{
+    private Camera _camera;
+    // Use this for initialization
+    void Start()
+    {
+        _camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(_camera.transform);
+    }
+}
