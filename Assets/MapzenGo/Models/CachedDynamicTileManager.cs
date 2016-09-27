36509707b5c29cbe1bde071493933500a0d88f8b
brnkhy <brnkahyaoglu@gmail.com>
2016-09-27 23:36:51 +0300

    remove threading from CachedDynamicTileManager
    didn't committed that intentionally in the first place

--- Assets/MapzenGo/Models/CachedDynamicTileManager.cs
+++ Assets/MapzenGo/Models/CachedDynamicTileManager.cs
@@ -29,51 +29,21 @@ namespace MapzenGo.Models
             base.Start();
         }
 
-        public override void Update()
-        {
-            base.Update();
-
-            lock (_readyToProcess)
-            {
-                if (_readyToProcess.Any())
-                {
-
-                    var tile = _readyToProcess.Dequeue();
-                    foreach (var factory in Plugins)
-                    {
-                        factory.Create(tile);
-                    }
-                }
-            }
-        }
-
         protected override void LoadTile(Vector2d tileTms, Tile tile)
         {
             var url = string.Format(_mapzenUrl, _mapzenLayers, Zoom, tileTms.x, tileTms.y, _mapzenFormat, _key);
             //this is temporary (hopefully), cant just keep adding stuff to filenames
-            var tilePath = Path.Combine(string.Format(CacheFolderPath, tile.Zoom), _mapzenLayers.Replace(',', '_') + "_" + tileTms.x + "_" + tileTms.y);
+            var tilePath = Path.Combine(CacheFolderPath, _mapzenLayers.Replace(',', '_') + "_" + tileTms.x + "_" + tileTms.y);
             if (File.Exists(tilePath))
             {
-                ThreadPool.QueueUserWorkItem((s) =>
+                using (var r = new StreamReader(tilePath, Encoding.Default))
                 {
-                    using (var r = new StreamReader((string)s, Encoding.Default))
-                    {
-                        var mapData = r.ReadToEnd();
-                        //ConstructTile(mapData, tile);
-                        var json = new JSONObject(mapData);
-                        if (!tile) // checks if tile still exists and haven't destroyed yet
-                            return;
-                        tile.Data = json;
-
-                        lock (_readyToProcess)
-                            _readyToProcess.Enqueue(tile);
-
-                    }
-                }, tilePath);
+                    var mapData = r.ReadToEnd();
+                    ConstructTile(mapData, tile);
+                }
             }
             else
             {
-                Debug.Log(url);
                 ObservableWWW.Get(url).Subscribe(
                     success =>
                     {
